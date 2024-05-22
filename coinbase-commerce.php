69111bd16b2826a2686baf6388dd82afa27d15a8
justin guyton <justin.guyton@coinbase.com>
2024-05-22 17:48:03 -0500

    declare extension (in)compatibility

--- coinbase-commerce.php
+++ coinbase-commerce.php
@@ -47,6 +47,11 @@ function cb_init_gateway() {
 		add_filter( 'woocommerce_email_order_meta_fields', 'cb_custom_woocommerce_email_order_meta_fields', 10, 3 );
 		add_filter( 'woocommerce_email_actions', 'cb_register_email_action' );
 		add_action( 'woocommerce_email', 'cb_add_email_triggers' );
+		add_action( 'before_woocommerce_init', function() {
+			if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+				\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+			}
+		} );
 	}
 }
 add_action( 'plugins_loaded', 'cb_init_gateway' );
